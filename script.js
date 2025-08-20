cd21118369cd71b05d8964be453c42413d186edf
Emily Kovacs <emilykovacs@outlook.com.br>
2025-08-19 22:54:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text)
 }
 
-// AIzaSyDurx_TiFXqRZhTfVnjK-2SmhaeXE1gqIY
 const perguntarIA = async (question, game, apiKey) => {
   const model = "gemini-2.5-flash"
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
